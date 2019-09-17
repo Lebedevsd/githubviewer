14b506b28a3fc4b500e4fee064f4da5e03befe1c
Sergey Lebedev <Lebedevsd@gmail.com>
2019-09-17 21:26:15 +0200

    initial commit

---
+++ app/src/main/java/com/lebedevsd/githubviewer/ui/main/MainFragment.kt
@@ -0,0 +1,27 @@
+package com.lebedevsd.githubviewer.ui.main
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.lebedevsd.githubviewer.R
+
+class MainFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = MainFragment()
+    }
+
+    private val viewModel by viewModels<MainViewModel>()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.main_fragment, container, false)
+    }
+
+
+}
